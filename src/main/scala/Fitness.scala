900f632baba6cd011bfb0da59107cd5dcc92a2e9
Emil Hellman <emil.hellman@gmail.com>
2015-08-08 18:50:26 +1200

    AggregateDepth and cleared lines fitness functions

--- src/main/scala/Fitness.scala
+++ src/main/scala/Fitness.scala
@@ -1,12 +1,12 @@
-import org.jgap.{FitnessFunction, IChromosome}
+import org.jgap.{ FitnessFunction, IChromosome }
 
-class CommandFitness(problem:Problem) extends FitnessFunction {
+class CommandFitness(problem: Problem) extends FitnessFunction {
   override def evaluate(chromosome: IChromosome): Double = {
     val builder = new StringBuilder
     val simulator = new Simulator(problem, 0)
     var i = 1
-    chromosome.getGenes foreach { g =>
-      if(!simulator.isGameOver) {
+    chromosome.getGenes foreach { g ⇒
+      if (!simulator.isGameOver) {
         simulator.play(Move.fromName(g.getAllele.toString))
         i += 1
         builder.append(g.getAllele.toString)
@@ -17,10 +17,27 @@ class CommandFitness(problem:Problem) extends FitnessFunction {
   }
 }
 
-class HeightFitness(weight:Double) extends Function[Simulator,Double] {
-  def apply(simulator:Simulator):Double = {
-    simulator.board.transpose.indexWhere { row =>
-      row.contains(true)
+class AggergateDepthFitness(weight: Double) extends Function[Array[Array[Boolean]], Double] {
+  def apply(board: Array[Array[Boolean]]): Double = {
+    board.foldLeft(0d) { (depth, col) ⇒
+      val firstIdx = col.indexOf(true)
+      if (firstIdx == -1) {
+        depth + col.length
+      } else {
+        depth + firstIdx
+      }
+    } * weight
+  }
+}
+
+class CompleteLinesFitness(weight: Double) extends Function[Array[Array[Boolean]], Double] {
+  def apply(board: Array[Array[Boolean]]): Double = {
+    board.transpose.foldLeft(0) { (numCleared, row) ⇒
+      if (row.forall(b => b)) {
+        numCleared + 1
+      } else {
+        numCleared
+      }
     } * weight
   }
 }
---
+++ src/test/scala/FitnessTests.scala
@@ -0,0 +1,106 @@
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+
+class FitnessTests extends FunSpec with ShouldMatchers {
+  describe("AggregateDepthFitness") {
+    describe("gives the sum of the depth") {
+      it("counts the max depth for each column when there is nothing on board") {
+        val board = Array(Array(false, false, false))
+        val f = new AggergateDepthFitness(1)
+        f.apply(board) shouldBe 3d
+      }
+
+      it("counts the depth as the first true it find in board for a column") {
+        val board = Array(Array(true, false, false))
+        val f = new AggergateDepthFitness(1)
+        f.apply(board) shouldBe 0d
+      }
+
+      it("counts the max depth as the bottom if that is firt true") {
+        val board = Array(Array(false, false, true))
+        val f = new AggergateDepthFitness(1)
+        f.apply(board) shouldBe 2d
+      }
+
+      it("counts the adds the depth for each column") {
+        val board = Array(
+          Array(false, false, false),
+          Array(false, false, true),
+          Array(false, true, false),
+          Array(true, false, false)
+        )
+        val f = new AggergateDepthFitness(1)
+        f.apply(board) shouldBe 6d
+      }
+
+      it("applies it's weight to the output") {
+        val board = Array(
+          Array(false, false, false),
+          Array(false, false, true),
+          Array(false, true, false),
+          Array(true, false, false)
+        )
+        val f = new AggergateDepthFitness(0.5)
+        f.apply(board) shouldBe 3d
+      }
+    }
+  }
+
+  describe("CompleteLinesFitness") {
+    describe("gives the number of rows that will be cleared") {
+      it("counts complete rows on an empty board as no cleard rows") {
+        val board = Array(Array(false, false, false))
+        val f = new CompleteLinesFitness(1)
+        f.apply(board) shouldBe 0d
+      }
+
+      it("counts as one if there is only true values on first row") {
+        val board = Array(Array(true, false, false))
+        val f = new CompleteLinesFitness(1)
+        f.apply(board) shouldBe 1d
+      }
+
+      it("counts as one if there is only true values on last row") {
+        val board = Array(Array(false, false, true))
+        val f = new CompleteLinesFitness(1)
+        f.apply(board) shouldBe 1d
+      }
+
+      it("counts as zero if there is false values on a row") {
+        val board = Array(Array(false), Array(false), Array(true))
+        val f = new CompleteLinesFitness(1)
+        f.apply(board) shouldBe 0d
+      }
+
+      it("counts as one if there is only true values on a row") {
+        val board = Array(Array(true), Array(true), Array(true))
+        val f = new CompleteLinesFitness(1)
+        f.apply(board) shouldBe 1d
+      }
+
+      it("adds the counts for several completed rows") {
+        val board = Array(
+          Array(true, false, true),
+          Array(true, false, true),
+          Array(true, false, true),
+          Array(true, false, true)
+        )
+        val f = new CompleteLinesFitness(1)
+        f.apply(board) shouldBe 2d
+      }
+
+      it("applies it's weight to the output") {
+        val board = Array(
+          Array(true, false, true),
+          Array(true, false, true),
+          Array(true, false, true),
+          Array(true, false, true)
+        )
+        val f = new CompleteLinesFitness(0.5)
+        f.apply(board) shouldBe 1d
+      }
+    }
+  }
+
+
+}
